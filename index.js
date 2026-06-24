16a47b472243491b53c8a3b66ecbf9c511e0a7d5
IURII VESELSKII <you@example.com>
2026-06-25 01:32:45 +0500

    Test: add divide function

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ function multiply(a, b) { return a * b; }
 function divide(a, b) { return a / b; }
 function test() { return true; }
 function test() { return true; }
+function divide(a, b) { return a / b; }
